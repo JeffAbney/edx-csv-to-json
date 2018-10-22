35f483a346e94f45d67c6b4bc32ce25ed330fc02
JeffAbney <jeffmabney@gmail.com>
2018-10-22 12:34:38 -0500

    add files

---
+++ csvToJason.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const csv = require('csvtojson')
+
+
+const convertCsvToJson = (csvFilePath = './customer-data.csv', jsonFilePath = './converted-data.json') => {
+  console.log('Converting file ', csvFilePath)
+  const newJson =
+    csv()
+    .on('done', () => console.log('Finished converting file.')) 
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+      fs.writeFileSync(path.join(__dirname, jsonFilePath), JSON.stringify(jsonObj,null,2))
+    })
+
+
+  
+}
+
+convertCsvToJson(process.argv[2], process.argv[3]);
